5d663b58c05db3e90b16980fa9c84f25571b0a32
Unrealrojo234 <otienotyan812@gmail.com>
2024-11-02 19:14:28 +0300

    First time commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { createClient } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+//import { useNavigate } from "react-router-dom";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+
+const supabaseUrl = "https://gtygqabysomtazoewxrq.supabase.co";
+const supabaseKey = import.meta.env.VITE_REACT_API_SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+//console.log(supabaseK
+export default function App() {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (!session) {
+    return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
+  } else {
+    return <div>Logged in!</div>;
+  }
+}
